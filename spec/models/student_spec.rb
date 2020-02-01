39ed45fd79a96397aba66cd5fd527ccd9e893a89
Jared S <jaredjames2020@gmail.com>
2020-02-01 18:57:32 +0000

    Automatically backed up by Learn

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -13,11 +13,5 @@ describe Student do
   it 'has a to_s instance method' do
     expect(@student.to_s).to eq("Daenerys Targaryen")
   end
- 
-  it 'can be created' do
-    post = Post.create!(title: "My title", description: "The post description")
-    expect(post).to be_valid
-  end
-end
 
 end
